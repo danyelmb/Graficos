3c998f14787e2f8ea556e6b8cbc466e3c665158a
danyelmb <36004911+danyelmb@users.noreply.github.com>
2018-02-27 07:51:45 +0100

    Add files via upload

--- Molino/main.cpp
+++ Molino/main.cpp
@@ -1,14 +1,16 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <stdio.h>
+#include <Windows.h>
 
 void initFunc();
 void funReshape(int w, int h);
 void funDisplay();
 void drawTriangulo(char color);
+void drawAspas();
 void funKeyboard(int key, int x, int y);
-void drawEsfera(char forma,char color);
 void repeat(int t);
+
 
 using namespace std;
 
@@ -19,6 +21,8 @@ GLfloat desZ = -5.0f;
 GLfloat rotY =  0.0f;
 GLfloat girar = 0.0f;
 
+
+
 int main(int argc, char** argv) {
 
  // Inicializamos GLUT
@@ -26,7 +30,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(w,h);
     glutInitWindowPosition(50,50);
-    glutCreateWindow("Sesion 4");
+    glutCreateWindow("Sesion 3");
     
  // Inicializamos GLEW
     GLenum err = glewInit();
@@ -43,7 +47,6 @@ int main(int argc, char** argv) {
     glutDisplayFunc(funDisplay);
     glutSpecialFunc(funKeyboard);
     glutTimerFunc(10, repeat, 0);
-    
  // Bucle principal
     glutMainLoop();
     
@@ -54,28 +57,16 @@ void initFunc() {
    
  // Test de profundidad
     glEnable(GL_DEPTH_TEST);
-    glPolygonOffset(1.0,1.0);
     
  // Modelo de sombreado
-    glShadeModel(GL_SMOOTH);
- /*   
- //Culling de las caras traseras
-    glFrontFace(GL_CCW);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-*/
+    glShadeModel(GL_FLAT);
     
- // Modo de rasterizado de las caras
-    glPolygonMode(GL_FRONT,GL_FILL);
-    glPolygonMode(GL_BACK,GL_LINE); 
- 
- // Simulación de niebla    
-    glEnable(GL_FOG);    
-    glFogi(GL_FOG_MODE,GL_LINEAR);
-    glFogf(GL_FOG_START, 10.0);
-    glFogf(GL_FOG_END  , 20.0);
-    GLfloat colorNiebla[4] = {0.0, 0.0, 0.0, 1.0};
-    glFogfv(GL_FOG_COLOR, colorNiebla);
+
+    
+    
+
+    
+    
     
     
 }
@@ -102,20 +93,9 @@ void funDisplay() {
     glLoadIdentity();
     
  // Matriz de Proyección P (Cámara)
-    //No deformar camara
-    //GLfloat aspectRatio = (GLfloat)w/(GLfloat)h;
-    //Tipos Camaras
-    //Configuracion 1
-    GLfloat fovy = 50.0f, aspectRatio = (GLfloat)w/(GLfloat)h, nplane = 1.0f, fplane = 20.0f;
+    GLfloat aspectRatio = (GLfloat)w/(GLfloat)h;    
+    GLfloat fovy = 50.0f, nplane = 0.1f, fplane = 20.0f;
     gluPerspective(fovy,aspectRatio,nplane,fplane);
-    
-    //Configuracion 2
-    //GLfloat left = -2.0f, right = 2.0f, bottom = -2.0f, top = 2.0f, nplane = 1.0f, fplane = 20.0f;
-    //Ortografica
-    //glOrtho(left,right,bottom,top,nplane,fplane);
-    //Perspectiva 1
-    //glFrustum(left,right,bottom,top,nplane,fplane);
-    
     
  // Para configurar las matrices M y V
     glMatrixMode(GL_MODELVIEW);  
@@ -123,64 +103,50 @@ void funDisplay() {
     
  // Matriz de Vista V (Cámara)
     // Aquí cargaremos la matriz V
-    GLfloat eye[3]    = {0.0,  2.0,  0.0};
-    GLfloat center[3] = {0.0,  0.0,  -5.0};
-    GLfloat up[3]     = {0.0,  1.0,  0.0};
-    gluLookAt(    eye[0],    eye[1],    eye[2],
-               center[0], center[1], center[2],
-                   up[0],     up[1],     up[2]);
 
- // Dibujamos los objetos (M)
-    glTranslatef(0.0f, 0.0f, desZ);
-    
+ // Dibujamos un triángulo
+    //glScalef(int, int, int);
+    glTranslatef( 0.0f, 0.0f, desZ);
+    glRotatef(rotY, 0.0f, 1.0f, 0.0f);
     glPushMatrix();
-        glScalef(1.0f,1.0f,1.0f);
-        glRotatef(girar,0.0f,1.0f, 0.0f);
-         glEnable(GL_POLYGON_OFFSET_FILL);
-            drawEsfera('w','g');
-         glDisable(GL_POLYGON_OFFSET_FILL);
+        glTranslatef( 0.0f, 1.5f, 0.0f);
+        glRotatef(girar, 0.0f, 0.0f, 1.0f);
+        drawAspas();
     glPopMatrix();
     glPushMatrix();
-        glScalef(1.0f,1.0f,1.0f);
-        glRotatef(girar,0.0f,1.0f, 0.0f);
-        drawEsfera('s','b');
+        glTranslatef( 0.0f, 1.5f, -0.051f);
+        glRotatef(90, 1.0f, 0.0f, 0.0f);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glutSolidCylinder(0.05f, 1.5f, 20, 5);
     glPopMatrix();
+
     
  // Intercambiamos los buffers
     glutSwapBuffers();
     
 }
 
-void drawEsfera(char forma,char color){
-    switch(color) {
-        case 'r':
-            glColor3f(1.0f, 0.0f, 0.0f);
-            break;
-        case 'g':
-            glColor3f(0.0f, 1.0f, 0.0f);
-            break;
-        case 'b':
-            glColor3f(0.0f, 0.0f, 1.0f);
-            break;
-        default:
-            glColor3f(1.0f, 1.0f, 1.0f);            
-    }
-    switch(forma){
-        case 's':
-            glutSolidSphere(1.0f,50,50);
-            break;
-        case 'w':
-            glutWireSphere(1.0f,50,50);
-            break;
-        default:
-            glutWireSphere(1.0f,50,50);
-    }
-
+void drawAspas(){
+    glPushMatrix();
+        glTranslatef(0.0f, -0.74f, 0.0f);
+        drawTriangulo('g');
+    glPopMatrix();
+    
+    glPushMatrix();
+        glRotatef(120, 0.0f, 0.0f, 1.0f);
+        glTranslatef( 0.0f, -0.74f, 0.0f);
+        drawTriangulo('r');
+    glPopMatrix();
+    
+    glPushMatrix();
+        glRotatef(240, 0.0f, 0.0f, 1.0f);
+        glTranslatef( 0.0f, -0.74f, 0.0f);
+        drawTriangulo('b');
+    glPopMatrix();
 }
 
-
 void drawTriangulo(char color) {
-    int var = 0;
+    
     switch(color) {
         case 'r':
             glColor3f(1.0f, 0.0f, 0.0f);
@@ -191,29 +157,16 @@ void drawTriangulo(char color) {
         case 'b':
             glColor3f(0.0f, 0.0f, 1.0f);
             break;
-        case 'm':
-            var=1;
-            break;
         default:
             glColor3f(1.0f, 1.0f, 1.0f);            
     }
     
-    if (var > 0){
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex3f( 0.5f, -0.5f, 0.0f);
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex3f( 0.0f,  0.5f, 0.0f);
-    }else{
-    
     glBegin(GL_TRIANGLES);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f( 0.5f, -0.5f, 0.0f);
-        glVertex3f( 0.0f,  0.5f, 0.0f);
+        glVertex3f(-0.2f, 0.0f, 0.0f);
+        glVertex3f( 0.25f, -0.3f, 0.0f);
+        glVertex3f( 0.0f,  0.75f, 0.0f);
     glEnd();
     
-    }
 }
 
 void funKeyboard(int key, int x, int y) {
@@ -238,11 +191,14 @@ void funKeyboard(int key, int x, int y) {
     
     glutPostRedisplay();
       
+    
+  
 }
 
 void repeat(int t){
-    girar += 0.5f ;
-    Sleep(10);
+    girar += 1.25f ;
+    //Sleep(10);
     glutPostRedisplay();
     glutTimerFunc(10, repeat, 0);
+ 
 }
